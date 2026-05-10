2d0e3edd87ff7efd4d4a3deec113f5c74e2e6fe7
brooksnithiwat <photogallery362@gmail.com>
2026-05-10 20:08:26 +0700

    check cd backend

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -6,6 +6,7 @@ const prisma = new PrismaClient()
 async function main() {
   const passwordHash = await bcrypt.hash('admin1234', 10)
 
+  
   await prisma.user.upsert({
     where: { email: 'admin@system.com' },
     update: {},
